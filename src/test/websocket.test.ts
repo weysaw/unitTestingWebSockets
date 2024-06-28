d890e2048b02779df788537c0a4f0ed0ef897d02
Axel L Ornelas M <axel.ornelas@inercy.com>
2024-06-28 11:25:56 -0700

    basic use of tdd

--- src/test/websocket.test.ts
+++ src/test/websocket.test.ts
@@ -1,19 +1,28 @@
-import ServerChat from '../server'
-import ClientChat from '../client'
+import { ServerChat } from '../server'
+import { ClientChat } from '../client'
 
 describe('websocket', (): void => {
     let server: ServerChat
     beforeAll((done: any): void => {
-       server = new ServerChat(8080)
-       server.startServer()
+        server = new ServerChat(8080)
+        server.startServer()
+        done() 
     })
 
-    afterAll((done) => {
-        server.close(done)
+    afterAll((done): void => {
+        server.close()
+        done()
     })
 
-    test('should echo messages to the client connected', (done): void => {
+    test('should echo messages received from server', (done: jest.DoneCallback): void => {
         const client = new ClientChat('ws://localhost:8080')
+        client.startConnection()
+        client.listenMessage((message: string): void => {
+            console.log(message)
+            expect(message).toBe('Hello I am the server, you sent: Hello I am the client')
+            client.close()
+            done()
+        })
 
     })
 })
